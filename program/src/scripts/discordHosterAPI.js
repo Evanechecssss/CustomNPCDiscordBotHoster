ce5ce9c8a91d94887ca2be654339b95dac10c89b
Evanechecssss <evan.ko@mail.ru>
2021-10-19 21:11:43 +0500

    main

--- program/src/scripts/discordHosterAPI.js
+++ program/src/scripts/discordHosterAPI.js
@@ -5,14 +5,23 @@
 var JAVA_URL = Java.type("java.net.URL")
 var JAVA_CHANNELS = Java.type("java.nio.channels.Channels")
 var JAVA_FILE_OUTPUT_STREAM = Java.type("java.io.FileOutputStream")
-
-
+var JAVA_PATH = Java.type("java.nio.file.Path")
+var JAVA_FILE = Java.type("java.io.File")
+var JAVA_FILES = Java.type("java.nio.file.Files")
+var JAVA_STANDART_COPY_OPTION = Java.type("java.nio.file.StandardCopyOption")
+var API = Java.type("noppes.npcs.api.NpcAPI").Instance()
+var WORLD_DIR = API.getWorldDir()
 function START() {
 
 }
 function DOWNLOAD_ASSETS() {
-    var website = new JAVA_URL("https://github.com/Evanechecssss/CustomNPCDiscordBotHoster");
-    rbc = JAVA_CHANNELS.newChannel(website.openStream());
-    var fos = new JAVA_FILE_OUTPUT_STREAM("README.md");
-    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+    var website = new JAVA_URL("https://raw.githubusercontent.com/Evanechecssss/CustomNPCDiscordBotHoster/main/testToSend.zip")
+    var target = JAVA_PATH.of(WORLD_DIR.getPath())
+    try {
+        var inputStream = website.openStream()
+        JAVA_FILES.copy(inputStream, target, JAVA_STANDART_COPY_OPTION.REPLACE_EXISTING)
+        target.toFile().renameTo(new JAVA_FILE("content.zip"))
+    } catch {
+        print("TEMP ERROR. HERE WILL BE EXCEPTION OBJECT")
+    }
 }
