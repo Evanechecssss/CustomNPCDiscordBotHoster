7b0feb6992d2ca08bf36754560d95cc3c356bf5a
Evanechecssss <evan.ko@mail.ru>
2021-10-20 22:47:39 +0500

    correct mistakes

--- program/src/scripts/discordHosterAPI.js
+++ program/src/scripts/discordHosterAPI.js
@@ -8,20 +8,23 @@ var JAVA_FILE_OUTPUT_STREAM = Java.type("java.io.FileOutputStream")
 var JAVA_PATH = Java.type("java.nio.file.Path")
 var JAVA_FILE = Java.type("java.io.File")
 var JAVA_FILES = Java.type("java.nio.file.Files")
+var JAVA_FILE_SYSTEM = Java.type("java.nio.file.FileSystems")
 var JAVA_STANDART_COPY_OPTION = Java.type("java.nio.file.StandardCopyOption")
 var API = Java.type("noppes.npcs.api.NpcAPI").Instance()
 var WORLD_DIR = API.getWorldDir()
 function START() {
-
 }
 function DOWNLOAD_ASSETS() {
     var website = new JAVA_URL("https://raw.githubusercontent.com/Evanechecssss/CustomNPCDiscordBotHoster/main/testToSend.zip")
-    var target = JAVA_PATH.of(WORLD_DIR.getPath())
+    var target = JAVA_FILE_SYSTEM.getDefault().getPath(WORLD_DIR.getPath() + "/content.zip")
     try {
         var inputStream = website.openStream()
         JAVA_FILES.copy(inputStream, target, JAVA_STANDART_COPY_OPTION.REPLACE_EXISTING)
-        target.toFile().renameTo(new JAVA_FILE("content.zip"))
-    } catch {
-        print("TEMP ERROR. HERE WILL BE EXCEPTION OBJECT")
+    } catch (e) {
+        print(e)
     }
+    print(target)
+}
+function interact(e) {
+    DOWNLOAD_ASSETS()
 }
