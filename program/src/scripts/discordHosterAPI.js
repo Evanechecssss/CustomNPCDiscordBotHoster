bb14ef780bbb5c0291c807a3113bb31aff578437
Evanechecssss <evan.ko@mail.ru>
2021-10-22 16:58:42 +0500

    add method for unziping file

--- program/src/scripts/discordHosterAPI.js
+++ program/src/scripts/discordHosterAPI.js
@@ -5,9 +5,10 @@
 var JAVA_URL = Java.type("java.net.URL")
 var JAVA_BYTE = Java.type("java.lang.Byte")
 var JAVA_MATH = Java.type("java.lang.Math")
+var JAVA_CHARSET = Java.type("java.nio.charset.Charset")
 var JAVA_CHANNELS = Java.type("java.nio.channels.Channels")
 var JAVA_FILE_OUTPUT_STREAM = Java.type("java.io.FileOutputStream")
-var JAVA_FILE_INPUT_STEAM = Java.type("java.io.FileInputStream")
+var JAVA_FILE_INPUT_STREAM = Java.type("java.io.FileInputStream")
 var JAVA_ZIP_INPUT_STREAM = Java.type("java.util.zip.ZipInputStream")
 var JAVA_BUF_OUTPUT_STREAM = Java.type("java.io.BufferedOutputStream")
 var JAVA_PATH = Java.type("java.nio.file.Path")
@@ -21,7 +22,7 @@ var WORLD_DIR = API.getWorldDir()
 function DOWNLOAD_ASSETS() {
     var website = new JAVA_URL("https://raw.githubusercontent.com/Evanechecssss/CustomNPCDiscordBotHoster/main/testToSend.zip")
     var target = JAVA_FILE_SYSTEM.getDefault().getPath(WORLD_DIR.getPath() + "/content.zip")
-    var folder = new JAVA_FILE(JAVA_FILE_SYSTEM.getDefault().getPath(WORLD_DIR.getPath() + "/content/"))
+    var folder = JAVA_FILE_SYSTEM.getDefault().getPath(WORLD_DIR.getPath() + "/content/")
     if (!folder.exists()) {
         folder.mkdir()
     }
@@ -29,46 +30,55 @@ function DOWNLOAD_ASSETS() {
         var inputStream = website.openStream()
         JAVA_FILES.copy(inputStream, target, JAVA_STANDART_COPY_OPTION.REPLACE_EXISTING)
     } catch (e) {
-        print("ECXEPTION0" + e)
     }
-    UNZIP_FILE(target, JAVA_FILE_SYSTEM.getDefault().getPath(WORLD_DIR.getPath() + "/content/"))
+    UNZIP(target.toString(), folder.toString())
+}
+/**
+ * 
+ * @param {String} fileZip 
+ * @param {String} destDir 
+ */
+function UNZIP(fileZip, folder) {
+    var destDir = new JAVA_FILE(folder);
+    var buffer = new Uint8Array(1024 * 1024 * 50);
+    var zis = new JAVA_ZIP_INPUT_STREAM(new JAVA_FILE_INPUT_STREAM(fileZip), JAVA_CHARSET.forName("windows-1251"));
+    var zipEntry = zis.getNextEntry();
+    while (zipEntry != null) {
+        var newZipFile = newZipFile(destDir, zipEntry);
+        if (zipEntry.isDirectory()) {
+            if (!newZipFile.isDirectory() && !newZipFile.mkdirs()) {
+                throw new IOException("Failed to create directory " + newZipFile);
+            }
+        } else {
+            var parent = newZipFile.getParentFile();
+            if (!parent.isDirectory() && !parent.mkdirs()) {
+                throw new IOException("Failed to create directory " + parent);
+            }
+            var fos = new JAVA_FILE_OUTPUT_STREAM(newZipFile);
+            var len;
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+            fos.close();
+        }
+        zipEntry = zis.getNextEntry();
+    }
+    zis.closeEntry();
+    zis.close();
+}
+function newZipFile(destinationDir, zipEntry) {
+    var destFile = new JAVA_FILE(destinationDir, zipEntry.getName());
+
+    var destDirPath = destinationDir.getCanonicalPath();
+    var destFilePath = destFile.getCanonicalPath();
+
+    if (!destFilePath.startsWith(destDirPath + JAVA_FILE.separator)) {
+        throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+    }
+
+    return destFile;
 }
 
-function UNZIP_FILE(zipFile, outputPath) {
-    try {
-        var zis = new JAVA_ZIP_INPUT_STREAM(JAVA_FILES.newInputStream(zipFile))
-        var entry = zis.getNextEntry()
-        print(entry)
-        while (entry != null) {
-            var newFilePath = outputPath.resolve(entry.getName())
-            if (entry.isDirectory()) {
-                JAVA_FILES.createDirectories(newFilePath)
-            } else {
-                if (!JAVA_FILES.exists(newFilePath.getParent())) {
-                    JAVA_FILES.createDirectories(newFilePath.getParent())
-                }
-                try {
-                    print("1" + entry.getName())
-                    var bos = JAVA_FILES.newOutputStream(outputPath.resolve(newFilePath))
-                    print("2" + entry.getName())
-                    var buffer = [JAVA_MATH.toIntExact(entry.getSize())]
-                    print("3" + entry.getName())
-                    var location
-                    while ((location = zis.read(buffer)) != -1) {
-                        print("4" + entry.getName())
-                        bos.write(buffer, 0, location)
-                    }
-                } catch (e) {
-                    print("ECXEPTION1" + e)
-                }
-            }
-            print("5" + entry.getName())
-            entry = zis.getNextEntry()
-        }
-    } catch (e) {
-        print("ECXEPTION2" + e)
-    }
-}
 function interact(e) {
     DOWNLOAD_ASSETS()
 }
